8124d26b783686783bf5955838129b402846e48c
Ayman Baioumy <70239925+Vulpes19@users.noreply.github.com>
2023-02-24 17:45:36 +0000

    finished lowest common ancestor in recursion

--- Lowest Common Ancestor of a Binary Search Tree/lowestCommonAnc.cpp
+++ Lowest Common Ancestor of a Binary Search Tree/lowestCommonAnc.cpp
@@ -19,47 +19,22 @@ TreeNode    *newNode(int x)
     return (node);
 }
 
-TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q)
-{
-	TreeNode *head = root;
-	TreeNode	*anc = nullptr;
-	bool ifp = false;
-	bool ifq = false;
-	if (root == nullptr)
-		return (nullptr);
-	stack<TreeNode*> st;
-	st.push(root);
-	while (!st.empty())
-	{
-		TreeNode* node = st.top();
-		st.pop();
-		// if ( node->val == 0 )
-		// 	exit(1);
-		// cout << node->val << " "; // process current node
-		if (node->right != nullptr) {
-			st.push(node->right); // push right child onto stack
-		}
-		if ( node->right != nullptr && node->right->val == q->val)
-		{
-			cout << "hello q\n";
-			anc = newNode(node->val);
-			ifq = true;
-		}
-		if (node->left != nullptr)
-			st.push(node->left); // push left child onto stack
-		if ( node->left != nullptr && node->left->val == p->val && anc && anc->val == node->val )
-		{
-			std::cout << "hello p\n";
-			ifp = true;
-		}
-		if ( ifq && ifp )
-			return ( node );
-	}
-	return (nullptr);
+TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+	if ( root == nullptr )
+		return ( nullptr );
+	if ( root->val == p->val || root->val == q->val )
+		return ( root );
+	TreeNode *lca_left = lowestCommonAncestor( root->left, p, q );
+	TreeNode *lca_right = lowestCommonAncestor( root->right, p, q );
+	if ( lca_left && lca_right )
+		return ( root );
+	return (lca_left == nullptr ) ? lca_right : lca_left;
 }
 
 int  main( void )
 {
+	// TreeNode *root = newNode(2);
+    // root->right = newNode(1);
 	TreeNode *root = newNode(6);
     root->left = newNode(2);
     root->right = newNode(8);
@@ -69,7 +44,7 @@ int  main( void )
     root->right->right = newNode(9);
     root->left->right->left = newNode(3);
     root->left->right->right = newNode(5);
-    TreeNode *ret = lowestCommonAncestor(root, root->left, root->left->right);
+    TreeNode *ret = lowestCommonAncestor(root, root->left->left, root->right->left);
     if (ret != nullptr)
 	cout << ret->val << endl;
 }
